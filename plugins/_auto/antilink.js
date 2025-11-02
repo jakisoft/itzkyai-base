71acc935e137d9cc49e07a18c3edf76f50393e54
Muhammad Zaki <162775830+jakisoft@users.noreply.github.com>
2025-11-02 22:25:47 +0700

    Update antilink.js

--- plugins/_auto/antilink.js
+++ plugins/_auto/antilink.js
@@ -1,8 +1,8 @@
-let handler = async (m, { kiicode, isAdmins }) => {
+let handler = async (m, { kiicode, isAdmins, botNumber }) => {
   const id = m.chat
   const db = global.db.data
   if (!db.groups[id] || !db.groups[id].antilink) return
-  if (isAdmins) return
+  if (isAdmins && botNumber) return
   const content = m.text || m.caption || ''
   if (!content) return
   const regex = /(chat\.whatsapp\.com\/[A-Za-z0-9]+)/i
